c2a555f492a582eb7aafdece8908fc8de29e7f08
Manish Kumar <114581035+manishkr1754@users.noreply.github.com>
2024-06-21 12:17:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ def addition(a,b,c):
 
 def subtraction(a,b,d):
     return a-b-d
-=======
-def subtraction(a,b,e):
-    return a-b-e
->>>>>>> c82ecb76ca26d4f7fddfc77c9e5d72a94f957a29
 
 def division(a,b)
     return a/b
